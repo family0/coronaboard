e2bf5d48fec3345dced8b1ddb75afbb3fe5ae33c
TJ <tj@itst.co.kr>
2023-04-02 15:17:48 +0900

    DB 접속 정보 변경

--- coronaboard-api/database/index.js
+++ coronaboard-api/database/index.js
@@ -4,8 +4,8 @@ const config = {
     host: process.env.MYSQL_HOST || 'localhost',
     port: 3306,
     database: 'coronaboard',
-    username: 'tj',
-    password: process.env.MYSQL_PASSWORD || '1a!A12341',
+    username: 'coronaboard',
+    password: process.env.MYSQL_PASSWORD || 'coronaboard',
 };
 
 const sequelize = new Sequelize(
