122175c2c462c3011e3f5496aba37de164c4958b
TJ <tj@itst.co.kr>
2023-04-30 14:26:49 +0900

    react-bootstrap 사용

---
+++ coronaboard-web/src/pages/bootstrap/button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Button, ButtonGroup } from 'react-bootstrap';
+
+export default function ButtonPage() {
+  return (
+    <Container>
+      <div>
+        <Button variant="primary">Primary</Button>
+      </div>
+      <hr/>
+      <ButtonGroup size="md">
+        <Button variant="primary">오늘</Button>
+        <Button variant="outline-primary">어제</Button>
+      </ButtonGroup>
+    </Container>
+  )
+}
